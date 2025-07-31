64bb9464f23f32d82e8a92a2dbe745333bcf0cad
Henrique-Schultz <valente.schultz@gmail.com>
2025-07-31 15:32:19 -0300

    all layers for author

---
+++ Controllers/AutoresController.cs
@@ -0,0 +1,60 @@
+using LaboratorioRestApi.Models;
+using LaboratorioRestApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LaboratorioRestApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AutoresController : ControllerBase
+    {
+        private readonly BibliotecaService _service;
+
+        public AutoresController(BibliotecaService service)
+        {
+            _service = service;
+        }
+
+        // GET: api/autores?sobrenome=Silva
+        [HttpGet]
+        public ActionResult<IEnumerable<Autor>> GetAutoresPorSobrenome([FromQuery] string sobrenome)
+        {
+            var autores = _service.BuscarAutoresPorSobrenome(sobrenome);
+            return Ok(autores);
+        }
+
+        // GET: api/autores/5
+        [HttpGet("{id}")]
+        public ActionResult<Autor> GetAutor(int id)
+        {
+            var autor = _service.BuscarAutorPorId(id);
+            if (autor == null)
+                return NotFound();
+
+            return Ok(autor);
+        }
+
+        // POST: api/autores
+        [HttpPost]
+        public IActionResult PostAutor([FromBody] Autor autor)
+        {
+            _service.AdicionarAutor(autor);
+            return CreatedAtAction(nameof(GetAutor), new { id = autor.Id }, autor);
+        }
+
+        // PUT: api/autores/5
+        [HttpPut("{id}")]
+        public IActionResult PutAutor(int id, [FromBody] Autor autor)
+        {
+            if (id != autor.Id)
+                return BadRequest("ID da URL não bate com o corpo da requisição.");
+
+            var autorExistente = _service.BuscarAutorPorId(id);
+            if (autorExistente == null)
+                return NotFound();
+
+            _service.AtualizarAutor(autor);
+            return NoContent();
+        }
+    }
+}
--- Models/Autor.cs
+++ Models/Autor.cs
@@ -1,8 +1,19 @@
-namespace LaboratorioRestApi.Models {
-    public class Autor {
+using System.ComponentModel.DataAnnotations;
+
+namespace LaboratorioRestApi.Models
+{
+    public class Autor
+    {
+        [Key]
         public int Id { get; set; }
-        public string PrimeiroNome { get; set; }
-        public string UltimoNome { get; set; }
-        
+
+        [Required]
+        public required string PrimeiroNome { get; set; }
+
+        [Required]
+        public required string UltimoNome { get; set; }
+
+        // Propriedade de navegação (1 autor → muitos livros)
+        public ICollection<Livro> Livros { get; set; } = [];
     }
 }
