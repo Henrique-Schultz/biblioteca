08512555d317ac5adc56fd1dfe8dfd06bf4724c9
Henrique-Schultz <valente.schultz@gmail.com>
2025-07-31 14:23:03 -0300

    Initial commit

---
+++ Models/Autor.cs
@@ -0,0 +1,8 @@
+namespace LaboratorioRestApi.Models {
+    public class Autor {
+        public int Id { get; set; }
+        public string PrimeiroNome { get; set; }
+        public string UltimoNome { get; set; }
+        
+    }
+}
